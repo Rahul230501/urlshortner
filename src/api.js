3d39215b84474c86f1614c4ad3f9bb91a90b574d
Rahul Kumar <rahulkumar.230501@gmail.com>
2025-12-07 00:29:16 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:3000"; // change if needed
+// const API_BASE = "http://localhost:3000"; // change if needed
 // const API_BASE = "https://urlshortnerbackend-1-0qnj.onrender.com/"; // change if needed
 
-// const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:3000";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:3000";
 export const api = axios.create({
   baseURL: API_BASE,
 });
