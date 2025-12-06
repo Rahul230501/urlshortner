84b9acad7ab26ddba660b1ef28f6a3897f95a920
Rahul Kumar <rahulkumar.230501@gmail.com>
2025-12-06 22:43:22 +0530

    updating api url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:3000"; // change if needed
+// const API_BASE = "http://localhost:3000"; // change if needed
+const API_BASE = "https://urlshortnerbackend-1-0qnj.onrender.com/"; // change if needed
 
 export const api = axios.create({
   baseURL: API_BASE,
